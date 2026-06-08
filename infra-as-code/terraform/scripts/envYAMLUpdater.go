c5e0ded6193142d88585f3a41368aa64299129be
Pasam6 <Prasad.pasam@beehyv.com>
2026-06-08 13:59:23 +0530

    update envYAMLUpdater.go

--- infra-as-code/terraform/scripts/envYAMLUpdater.go
+++ infra-as-code/terraform/scripts/envYAMLUpdater.go
@@ -50,7 +50,7 @@ func main() {
 		os.Exit(1)
 	}
 	// Read the YAML file
-	yamlBytes, err := ioutil.ReadFile("../../../deploy-as-code/charts/environments/env.yaml")
+	yamlBytes, err := ioutil.ReadFile("../../../deploy-as-code/helm/environments/env.yaml")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading YAML file: %v\n", err)
 		os.Exit(1)
@@ -67,7 +67,7 @@ func main() {
 	// Write the updated YAML to stdout
 	fmt.Println(output)
 
-	err = ioutil.WriteFile("../../../deploy-as-code/charts/environments/env.yaml", []byte(output), 0644)
+	err = ioutil.WriteFile("../../../deploy-as-code/helm/environments/env.yaml", []byte(output), 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error writing YAML file: %v\n", err)
 		os.Exit(1)
