397466f8e6bc86f641e0b21245c7f3668639903e
Pasam6 <Prasad.pasam@beehyv.com>
2026-06-09 11:46:34 +0530

    update repo

--- infra-as-code/terraform/scripts/envYAMLUpdater.go
+++ infra-as-code/terraform/scripts/envYAMLUpdater.go
@@ -67,7 +67,7 @@ func main() {
 	// Write the updated YAML to stdout
 	fmt.Println(output)
 
-	err = ioutil.WriteFile("../../../deploy-as-code/helm/environments/env.yaml", []byte(output), 0644)
+	err = ioutil.WriteFile("../../../deploy-as-code/charts/environments/env.yaml", []byte(output), 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error writing YAML file: %v\n", err)
 		os.Exit(1)
